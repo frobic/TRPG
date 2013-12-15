47fe6d2a379e3c6ba488f1dbbf36bf41a670764f
Noiralos <thomasmerlyalpa@hotmail.fr>
2013-12-15 14:46:44 +0100

    stats.js

---
+++ TRPG-HTLM/js/stats.js
@@ -0,0 +1,32 @@
+stats = {
+	"GUE" : { 
+		"att" : 5, 
+		"hp" : 10,
+		"hpmax" : 15,
+		"cel" : 30,
+		"tag" : "GUE",
+		"rad" : 1,
+		"range" : 1,
+		"name" : "Guerrier"
+	},
+	"SCO" : {
+		"att" : 3,
+		"hp" : 6,
+		"hpmax" : 10,
+		"cel" : 20,
+		"tag" : "SCO",
+		"rad" : 2,
+		"range" : 1,
+		"name" : "Eclaireur"
+	},
+	"ARC" : {
+		"att" : 4, 
+		"hp" : 8, 
+		"hpmax" : 10,
+		"cel" : 40,
+		"tag" : "ARC",
+		"rad" : 1,
+		"range" : 3,
+		"name" : "Archer"
+	}
+}
